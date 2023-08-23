bb714f46ee70c7d53a46d1249d24f7f46b6e51f2
angel <angel.zazueta14@hotmail.com>
2023-08-22 22:28:32 -0700

    Initial commit

---
+++ src/NumeroNegativoExcepcion.java
@@ -0,0 +1,11 @@
+public class NumeroNegativoException extends Exception {
+    public NumeroNegativoException(){
+        super();
+    }
+    public NumeroNegativoException(String mensaje){
+        super(mensaje);
+    }
+}
+public class Main2{
+
+}
