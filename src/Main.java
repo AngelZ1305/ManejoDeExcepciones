5f359236ba3e6f6d87b453c216e54b9496bf7372
angel <angel.zazueta14@hotmail.com>
2023-08-22 22:29:43 -0700

    Excepciones de limites de cadena y de numeros negativos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static char caracterEn(String palabra, int posicion) throws Exception{
+        int longitudPalabra = palabra.length();
+        if (posicion >= 0 && posicion < longitudPalabra){
+            return palabra.charAt(posicion);
+        }
+        else {
+            throw new Exception("La posición esta fuera de los limites de la cadena");
+        }
     }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String lectTeclado = sc.nextLine();
+        System.out.println("Introduce una palabra: ");
+        try {
+            char caracter7 = caracterEn(lectTeclado, 7);
+            System.out.println("El caracter 7 de la palabra " + lectTeclado + " es: "+caracter7);
+        }catch (Exception e){
+            System.out.println("Has intentado recuperar una posición de la cadena de caracteres que no existe");
+        }
+    }
+
 }
--- src/NumeroNegativoExcepcion.java
+++ src/NumeroNegativoExcepcion.java
@@ -1,11 +1,26 @@
-public class NumeroNegativoException extends Exception {
-    public NumeroNegativoException(){
-        super();
-    }
+import java.util.Scanner;
+
+class NumeroNegativoException extends Exception {
     public NumeroNegativoException(String mensaje){
         super(mensaje);
     }
 }
-public class Main2{
+public class NumeroNegativoExcepcion{
+    public static void main(String[] args) {
 
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Ingresa un valor númerico:");
+            double numero = sc.nextDouble();
+
+            if (numero >= 0) {
+                double raizCuadrada = Math.sqrt(numero);
+                System.out.println("La raiz cuadra del número es: " + raizCuadrada);
+            } else {
+                throw new NumeroNegativoException("Introduciste un número negativo");
+            }
+        } catch (NumeroNegativoException e) {
+            System.out.println(e.getMessage());
+
+        }
+    }
 }
